7b2268a77c8fa96f71498cdd02ed910f4cc5e924
Suman Kisku <sumankisku1@gmail.com>
2021-01-23 23:47:14 +0530

    Install gatsby-plugin-postcss

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     "gatsby-plugin-sitemap",
     "gatsby-plugin-mdx",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-filesystem",
       options: {

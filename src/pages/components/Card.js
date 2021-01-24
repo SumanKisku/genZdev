85e15c3827c1e60ec9908b643bfa45bb47760a6c
Suman Kisku <sumankisku1@gmail.com>
2021-01-24 09:32:06 +0530

    Change details, add utility classes

--- src/pages/components/Card.js
+++ src/pages/components/Card.js
@@ -2,12 +2,15 @@ import React from "react"
 
 export default function Feature(props) {
   return (
-    <div className="card text-gray-100 border border-gray-100 px-4 py-6 pr-8 relative">
-      <div className="w-full h-full border border-gray-100 bg-black absolute"></div>
+    <div className="card text-gray-100 border border-gray-100 rounded-3xl h-48 px-8 py-6 pr-8 flex flex-col relative">
+      {/* <div className="w-full h-full border border-gray-100 bg-black absolute"></div> */}
       <h4 className="text-2xl font-bold">{props.heading}</h4>
       <ul className="mt-2 list-disc">
           {props.list.map((elem) => <li className="ml-5">{elem}</li>)}
       </ul>
+      <div className="transform translate-x-6 absolute bottom-3">
+        <span>{props.price}</span>
+      </div>
       
     </div>
   )

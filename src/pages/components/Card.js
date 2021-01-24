d24b7413e8f013cdca49a4940466ded1089018bc
Suman Kisku <sumankisku1@gmail.com>
2021-01-24 09:38:03 +0530

    Add margin top

--- src/pages/components/Card.js
+++ src/pages/components/Card.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Feature(props) {
   return (
-    <div className="card text-gray-100 border border-gray-100 rounded-3xl h-48 px-8 py-6 pr-8 flex flex-col relative">
+    <div className="card text-gray-100 border border-gray-100 rounded-3xl h-48 mt-8 px-8 py-6 pr-8 flex flex-col relative">
       {/* <div className="w-full h-full border border-gray-100 bg-black absolute"></div> */}
       <h4 className="text-2xl font-bold">{props.heading}</h4>
       <ul className="mt-2 list-disc">

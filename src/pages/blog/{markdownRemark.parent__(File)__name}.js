fe936aa5490ea45dd70f228fcc2d360fdabe9efd
Suman Kisku <sumankisku1@gmail.com>
2021-01-30 18:58:35 +0530

    Style blog template

--- src/pages/blog/{markdownRemark.parent__(File)__name}.js
+++ src/pages/blog/{markdownRemark.parent__(File)__name}.js
@@ -1,12 +1,17 @@
 import React from "react"
+import Navigation from '../components/Navigation'
 import { graphql } from "gatsby"
 
 export default function Post({data}) {
   return (
-      <main>
-      <h1 className="text-gray-100">{data.markdownRemark.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
-    </main>
+      <div className="overflow-hidden">
+        <Navigation></Navigation>
+        <div className="w-11/12 md:w-8/12 mx-auto mt-2">
+        <h1 className="text-gray-100 text-5xl mb-2 md:text-6xl">{data.markdownRemark.frontmatter.title}</h1>
+        <h2 className="text-gray-100 font-bold mb-4">{data.markdownRemark.frontmatter.date}</h2>
+        <div className="text-lg" dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        </div>
+      </div>
   )
 }
 
@@ -15,6 +20,7 @@ query($id: String!) {
     markdownRemark(id: {eq: $id}) {
       frontmatter {
         title
+        date
       }
       html
     }
